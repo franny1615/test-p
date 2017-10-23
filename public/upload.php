e0b3c0c43444986fbc6f7bc6755668e1d5cb81e2
franny1615 <31251485+franny1615@users.noreply.github.com>
2017-10-22 17:05:36 -0700

    Update upload.php

--- public/upload.php
+++ public/upload.php
@@ -26,17 +26,3 @@
     }
  }
 ?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Rar</title>
-  </head>
-  <body>
-    <form action="" method="post" enctype="multipart/form-data">
-    		Select image to upload:
-    		<input type="file" name="fileToUpload" id="fileToUpload">
-    		<input type="submit" value="Upload Image" name="submit">
-	  </form>
-  </body>
-</html>
