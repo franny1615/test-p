3727f6823714a73cf008597362090e5a6db60d73
franny1615 <31251485+franny1615@users.noreply.github.com>
2017-10-22 17:24:14 -0700

    Update upload.php

--- app/upload.php
+++ app/upload.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Upload your files</title>
-</head>
-<body>
-  <form enctype="multipart/form-data" action="upload.php" method="POST">
-    <p>Upload your file</p>
-    <input type="file" name="uploaded_file"></input><br />
-    <input type="submit" value="Upload"></input>
-  </form>
-</body>
-</html>
-
 <?php
   if(!empty($_FILES['uploaded_file']))
   {
